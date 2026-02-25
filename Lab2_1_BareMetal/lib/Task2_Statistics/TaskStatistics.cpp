3953b6db2ceada12ac6e13abab72ea7533fd11bb
dmracovit <dumitru.racovitsa@gmail.com>
2026-02-25 21:02:48 +0200

    fixed bug with invisible click

--- Lab2_1_BareMetal/lib/Task2_Statistics/TaskStatistics.cpp
+++ Lab2_1_BareMetal/lib/Task2_Statistics/TaskStatistics.cpp
@@ -12,35 +12,34 @@ static int16_t blinkTogglesRemaining = 0;
 
 void TaskStatistics_Run(void)
 {
-    switch (state)
+    // Check for new press in ANY state (don't miss presses during blinking)
+    if (Signals_GetNewPressFlag())
     {
-    case STATE_IDLE:
-        if (Signals_GetNewPressFlag())
+        uint16_t duration = Signals_GetLastPressDuration();
+        bool isShort = Signals_GetIsShortPress();
+
+        Signals_SetTotalPresses(Signals_GetTotalPresses() + 1);
+
+        if (isShort)
         {
-            uint16_t duration = Signals_GetLastPressDuration();
-            bool isShort = Signals_GetIsShortPress();
-
-            Signals_SetTotalPresses(Signals_GetTotalPresses() + 1);
-
-            if (isShort)
-            {
-                Signals_SetShortPresses(Signals_GetShortPresses() + 1);
-                Signals_SetTotalShortDuration(Signals_GetTotalShortDuration() + duration);
-                blinkTogglesRemaining = 10; // 5 blinks = 10 toggles
-            }
-            else
-            {
-                Signals_SetLongPresses(Signals_GetLongPresses() + 1);
-                Signals_SetTotalLongDuration(Signals_GetTotalLongDuration() + duration);
-                blinkTogglesRemaining = 20; // 10 blinks = 20 toggles
-            }
-
-            Signals_SetNewPressFlag(false);
-            state = STATE_BLINKING;
+            Signals_SetShortPresses(Signals_GetShortPresses() + 1);
+            Signals_SetTotalShortDuration(Signals_GetTotalShortDuration() + duration);
+            blinkTogglesRemaining = 10; // 5 blinks = 10 toggles
         }
-        break;
+        else
+        {
+            Signals_SetLongPresses(Signals_GetLongPresses() + 1);
+            Signals_SetTotalLongDuration(Signals_GetTotalLongDuration() + duration);
+            blinkTogglesRemaining = 20; // 10 blinks = 20 toggles
+        }
 
-    case STATE_BLINKING:
+        Signals_SetNewPressFlag(false);
+        state = STATE_BLINKING;
+    }
+
+    // Handle blinking
+    if (state == STATE_BLINKING)
+    {
         LedDriver_YellowToggle();
         blinkTogglesRemaining--;
 
@@ -49,6 +48,5 @@ void TaskStatistics_Run(void)
             LedDriver_YellowOff();
             state = STATE_IDLE;
         }
-        break;
     }
 }
